98eabae0567373cacb2f9e470bc8def0a78754e2
Deep Saurabh Singh <deepglb@gmail.com>
2023-04-05 07:49:22 +0530

    Update countdown.js

--- recursion/countdown.js
+++ recursion/countdown.js
@@ -1,1 +1,22 @@
+// Countdown using recursion
+function countDown(n){
+    if(n <=0){
+        console.log('Done');
+        return;
+    }
+    console.log(n);
+    
+    n--;
+    countDown(n);
+}
 
+
+// Countdown iterative
+
+function countDown(n){
+    for(var i=n; i>0;i--){
+        console.log(i);
+    }
+    console.log("Done")
+
+}
