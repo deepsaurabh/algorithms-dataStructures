31e8531447cbb68899797917bc282ba66ebc46e9
Deep Saurabh Singh <deepglb@gmail.com>
2023-04-05 07:52:50 +0530

    Update factorial.js

--- recursion/factorial.js
+++ recursion/factorial.js
@@ -1,4 +1,13 @@
-// Factorial of a number
+// Factorial using recursion
+function factorial(num){
+    if(num ==1){
+        //return 1;
+    }
+
+    return num * factorial(num -1);
+}
+
+// Factorial of a number itertive way
 function factorial(num){
     let total = 1;
     for(let i=num;i>0;i--){
