9a1fd79cd7302fbc2830f146a07c31fdd2d8f308
Deep Saurabh Singh <deepglb@gmail.com>
2023-04-05 07:55:17 +0530

    Create collectOddValues.js

---
+++ recursion/collectOddValues.js
@@ -0,0 +1,22 @@
+// Helper method recursion
+function collectOddValues(arr){
+
+let result =[];
+
+    function helper(input){
+
+        if(input.length === 0){
+            return;
+        }
+
+        if(input[0] % 2 !== 0){
+            result.push(input[0]);
+        }
+        
+        helper(input.slice(1));
+    }
+
+    helper(arr);
+
+    return result;
+}
